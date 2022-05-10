c6efe08914f22731136d15764b76a00d78d272a2
Anirudh Uppunda <abhat38@gmail.com>
2022-05-10 13:49:04 +0530

    refactor bottom bar

--- app/src/main/java/com/abhat/bookhighlights/MainActivity.kt
+++ app/src/main/java/com/abhat/bookhighlights/MainActivity.kt
@@ -3,13 +3,26 @@ package com.abhat.bookhighlights
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import com.abhat.bookhighlights.ui.BottomBar
+import com.abhat.bookhighlights.ui.theme.BookHighlightsComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            BookHighlightsComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Scaffold(
+                        bottomBar = { BottomBar() }
+                    ) {
 
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/abhat/bookhighlights/ui/BottomBar.kt
+++ app/src/main/java/com/abhat/bookhighlights/ui/BottomBar.kt
@@ -1,47 +1,59 @@
 package com.abhat.bookhighlights.ui
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.abhat.bookhighlights.ui.theme.BookHighlightsComposeTheme
 
 @Composable
 fun BottomBar() {
-    var selectedState = remember {
-        "Highlight"
+    var selectedState = rememberSaveable {
+        mutableStateOf("Highlights")
     }
 
     BottomAppBar(
-        elevation = 8.dp,
-        content = {
-            Row {
-                BottomNavigationItem(
-                    icon = {
-                        //Icon(Icons.Filled.Favorite , "")
-                    },
-                    label = { Text(text = "Highlights") },
-                    selected = selectedState == "Highlights",
-                    onClick = {
-                        selectedState = "Highlights"
-                    },
-                    alwaysShowLabel = true
-                )
+        elevation = 12.dp,
+    ) {
+        BottomNavigationItem(
+            icon = {
+                //Icon(Icons.Filled.Favorite , "")
+            },
+            label = { Text(text = "Highlights") },
+            selectedContentColor= Color.Black,
+            unselectedContentColor= Color.Black.copy(alpha = 0.4f),
+            selected = selectedState.value == "Highlights",
+            onClick = {
+                selectedState.value = "Highlights"
+            },
+            alwaysShowLabel = true
+        )
 
-                BottomNavigationItem(
-                    icon = {
-                        //Icon(Icons.Filled.Favorite , "")
-                    },
-                    label = { Text(text = "Starred") },
-                    selected = selectedState == "Starred",
-                    onClick = {
-                        selectedState = "Starred"
-                    },
-                    alwaysShowLabel = true
-                )
-            }
-        }
-    )
+        BottomNavigationItem(
+            icon = {
+                //Icon(Icons.Filled.Favorite , "")
+            },
+            label = { Text(text = "Starred") },
+            selectedContentColor= Color.Black,
+            unselectedContentColor= Color.Black.copy(alpha = 0.4f),
+            selected = selectedState.value == "Starred",
+            onClick = {
+                selectedState.value = "Starred"
+            },
+            alwaysShowLabel = true
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BookHighlightsComposeTheme {
+        BottomBar()
+    }
 }
