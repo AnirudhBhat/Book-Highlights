4b5860d78902b985b49e2a3a633c32aced744629
Anirudh Uppunda <abhat38@gmail.com>
2022-05-13 16:00:40 +0530

    Handle media access runtime permission

--- app/src/main/java/com/abhat/bookhighlights/MainActivity.kt
+++ app/src/main/java/com/abhat/bookhighlights/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.abhat.bookhighlights
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import com.abhat.anumathi.PermissionResult
+import com.abhat.anumathi.anumathi
+import com.abhat.anumathi.registerPermissions
+import com.abhat.bookhighlights.bookslist.BooksListViewModel
+import com.abhat.bookhighlights.bookslist.BooksListViewModel.Event.CheckStoragePermission
 import com.abhat.bookhighlights.ui.BottomBar
 import com.abhat.bookhighlights.ui.theme.BookHighlightsComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    val viewModel: BooksListViewModel by viewModels()
+    private val registerPermission = registerPermissions()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        observeEvents()
         setContent {
             BookHighlightsComposeTheme {
                 // A surface container using the 'background' color from the theme
@@ -21,6 +35,37 @@ class MainActivity : ComponentActivity() {
                     ) {
 
                     }
+                }
+            }
+        }
+    }
+
+    private fun observeEvents() {
+        viewModel.event.observe(this, Observer { event ->
+            when (event) {
+                CheckStoragePermission -> {
+                    lifecycleScope.launch {
+                        val result = anumathi(
+                            registerPermission,
+                            arrayOf(
+                                Manifest.permission.ACCESS_MEDIA_LOCATION
+                            )
+                        )
+                        handlePermissionResult(result)
+                    }
+                }
+            }
+        })
+    }
+
+    private fun handlePermissionResult(result: Map<String, PermissionResult>) {
+        result.map { (permission, state) ->
+            when (state) {
+                is PermissionResult.Denied -> {
+
+                }
+                PermissionResult.Granted -> {
+
                 }
             }
         }
--- app/src/main/java/com/abhat/bookhighlights/bookslist/BooksListViewModel.kt
+++ app/src/main/java/com/abhat/bookhighlights/bookslist/BooksListViewModel.kt
@@ -1,6 +1,7 @@
 package com.abhat.bookhighlights.bookslist
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.abhat.bookhighlights.SingleLiveEvent
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -8,7 +9,8 @@ import kotlinx.coroutines.flow.asStateFlow
 
 class BooksListViewModel(
     private val savedStateHandle: SavedStateHandle,
-) {
+) : ViewModel() {
+
     private val booksUiState: MutableStateFlow<BooksListUIState> = MutableStateFlow(BooksListUIState.Loading)
     val viewState: StateFlow<BooksListUIState> = booksUiState.asStateFlow()
 
