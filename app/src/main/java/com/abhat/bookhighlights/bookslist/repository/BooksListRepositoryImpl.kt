482dccd2d4bda1f9ca8253a27461a5f734e66592
Anirudh Uppunda <abhat38@gmail.com>
2022-05-18 13:32:11 +0530

    add books list repository implementation.

--- app/src/main/java/com/abhat/bookhighlights/bookslist/repository/BooksListRepositoryImpl.kt
+++ app/src/main/java/com/abhat/bookhighlights/bookslist/repository/BooksListRepositoryImpl.kt
@@ -1,13 +1,28 @@
 package com.abhat.bookhighlights.bookslist.repository
 
-import com.abhat.bookhighlights.bookslist.repository.model.BooksListResponse
+import com.abhat.bookhighlights.bookslist.data.api.BooksListApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class BooksListRepositoryImpl: BooksListRepository {
-    override suspend fun getBooksList(title: String): Flow<BooksListResponse> {
+class BooksListRepositoryImpl(
+    private val booksListApi: BooksListApi
+): BooksListRepository {
+    override suspend fun getBookDetails(title: String): Flow<BooksListRepoState> {
         return flow {
-
+            val result = booksListApi.getBooksListAsync(title).await()
+            if (!result.items.isNullOrEmpty()) {
+                emit(
+                    BooksListRepoState.Success(
+                        booksList = result.items
+                    )
+                )
+            } else {
+                emit(
+                    BooksListRepoState.Error(
+                        error = Throwable("Didn't find any books with that title!")
+                    )
+                )
+            }
         }
     }
 }
