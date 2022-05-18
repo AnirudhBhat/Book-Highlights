314e9af56eb2841b9f440252d4396c9440b6e1bb
Anirudh Uppunda <abhat38@gmail.com>
2022-05-18 13:31:53 +0530

    add books list repo state model

--- app/src/main/java/com/abhat/bookhighlights/bookslist/repository/BooksListRepoState.kt
+++ app/src/main/java/com/abhat/bookhighlights/bookslist/repository/BooksListRepoState.kt
@@ -1,9 +1,9 @@
 package com.abhat.bookhighlights.bookslist.repository
 
-import com.abhat.bookhighlights.bookslist.repository.model.BooksListResponse
+import com.abhat.bookhighlights.bookslist.repository.model.Items
 
 sealed class BooksListRepoState {
-    data class Success(val news: BooksListResponse): BooksListRepoState()
+    data class Success(val booksList: List<Items>): BooksListRepoState()
     data class Error(
         val error: Throwable?
     ): BooksListRepoState()
