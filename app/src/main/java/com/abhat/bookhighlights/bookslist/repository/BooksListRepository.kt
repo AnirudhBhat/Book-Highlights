f5c2087cf40efe5e7d82f7850efb15815b5aa47d
Anirudh Uppunda <abhat38@gmail.com>
2022-05-18 13:31:04 +0530

    add BooksListRepository.kt interface

--- app/src/main/java/com/abhat/bookhighlights/bookslist/repository/BooksListRepository.kt
+++ app/src/main/java/com/abhat/bookhighlights/bookslist/repository/BooksListRepository.kt
@@ -1,8 +1,7 @@
 package com.abhat.bookhighlights.bookslist.repository
 
-import com.abhat.bookhighlights.bookslist.repository.model.BooksListResponse
 import kotlinx.coroutines.flow.Flow
 
 interface BooksListRepository {
-    suspend fun getBooksList(title: String): Flow<BooksListResponse>
+    suspend fun getBookDetails(title: String): Flow<BooksListRepoState>
 }
