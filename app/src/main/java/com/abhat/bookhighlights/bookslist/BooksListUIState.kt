cce7f12ae6bb825783731e2f195623e849389e45
Anirudh Uppunda <abhat38@gmail.com>
2022-05-18 13:34:13 +0530

    add "update book state" field for the books list ui state

--- app/src/main/java/com/abhat/bookhighlights/bookslist/BooksListUIState.kt
+++ app/src/main/java/com/abhat/bookhighlights/bookslist/BooksListUIState.kt
@@ -5,11 +5,14 @@ import com.abhat.bookhighlights.bookslist.model.Book
 sealed class BooksListUIState(
     val isLoading: Boolean = false,
     open val booksList: List<Book> = emptyList(),
+    open val book: Book? = null,
     open val error: Throwable? = null
 ) {
     object Loading : BooksListUIState(isLoading = true)
 
     data class Success(override val booksList: List<Book>) : BooksListUIState()
+
+    data class UpdateBookDetails(override val book: Book?) : BooksListUIState()
 
     data class Error(
         override val error: Throwable
