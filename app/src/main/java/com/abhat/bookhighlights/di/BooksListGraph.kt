09e8462e80e6004780f251436594fb72818942a8
Anirudh Uppunda <abhat38@gmail.com>
2022-05-18 13:32:58 +0530

    add di package and files which support retrofit operations

---
+++ app/src/main/java/com/abhat/bookhighlights/di/BooksListGraph.kt
@@ -0,0 +1,39 @@
+package com.abhat.bookhighlights.di
+
+import androidx.lifecycle.ViewModelStoreOwner
+import com.abhat.bookhighlights.bookslist.data.api.BooksListApi
+import com.abhat.bookhighlights.bookslist.repository.BooksListRepository
+import com.abhat.bookhighlights.bookslist.repository.BooksListRepositoryImpl
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by Anirudh Uppunda on 27,October,2020
+ */
+interface BooksListGraph {
+    val booksListRepository: BooksListRepository
+}
+
+class NetworkGraphImpl(viewModelStoreOwner: ViewModelStoreOwner) : BooksListGraph {
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(NetworkConstants.BASE_URL)
+        .client(okHttpClient)
+        .build()
+
+
+
+    override val booksListRepository: BooksListRepository = BooksListRepositoryImpl(retrofit.create(BooksListApi::class.java))
+}
---
+++ app/src/main/java/com/abhat/bookhighlights/di/NetworkConstants.kt
@@ -0,0 +1,8 @@
+package com.abhat.bookhighlights.di
+
+/**
+ * Created by Anirudh Uppunda on 19,October,2020
+ */
+object NetworkConstants {
+    const val BASE_URL = "https://www.googleapis.com/books/v1/" // Add your base url for retrofit here
+}
