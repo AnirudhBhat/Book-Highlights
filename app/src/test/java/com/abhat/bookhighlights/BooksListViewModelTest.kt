db1cda07524fe213e0912da72994f93bfb6ec31c
Anirudh Uppunda <abhat38@gmail.com>
2022-05-18 16:20:46 +0530

    add tests for GetBookDetails event

--- app/src/test/java/com/abhat/bookhighlights/BooksListViewModelTest.kt
+++ app/src/test/java/com/abhat/bookhighlights/BooksListViewModelTest.kt
@@ -5,15 +5,18 @@ import androidx.lifecycle.SavedStateHandle
 import com.abhat.bookhighlights.bookslist.BooksListUIState
 import com.abhat.bookhighlights.bookslist.BooksListViewModel
 import com.abhat.bookhighlights.bookslist.BooksListViewModel.Event.CheckStoragePermission
+import com.abhat.bookhighlights.bookslist.BooksListViewModel.Event.GetBookDetails
 import com.abhat.bookhighlights.bookslist.BooksListViewModel.Event.ParseBooksFromStorage
 import com.abhat.bookhighlights.bookslist.parser.BooksParser
 import com.abhat.bookhighlights.bookslist.parser.Parser
 import com.abhat.bookhighlights.bookslist.model.Book
 import com.abhat.bookhighlights.bookslist.model.Highlight
 import com.abhat.bookhighlights.bookslist.model.HtmlDocument
+import com.abhat.bookhighlights.bookslist.repository.BooksListRepository
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Rule
@@ -23,6 +26,7 @@ class BooksListViewModelTest {
     private lateinit var viewModel: BooksListViewModel
     private lateinit var savedStateHandle: SavedStateHandle
     private lateinit var booksParser: Parser
+    private lateinit var booksListRepository: BooksListRepository
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
@@ -31,7 +35,8 @@ class BooksListViewModelTest {
     fun setup() {
         savedStateHandle = SavedStateHandle()
         booksParser = mock()
-        viewModel = BooksListViewModel(savedStateHandle, BooksParser(booksParser))
+        booksListRepository = mock()
+        viewModel = BooksListViewModel(savedStateHandle, booksListRepository, BooksParser(booksParser))
 
     }
 
@@ -61,102 +66,153 @@ class BooksListViewModelTest {
 
     @Test
     fun `given storage permission granted, when books found on storage, then success state is shown`() {
-        whenever(booksParser.parseHtml(any())).thenReturn(
-            mutableListOf(
-                HtmlDocument(
-                    heading = listOf(
-                        "Highlight (yellow) - heading 1 book 1",
-                        "Highlight (yellow) - heading 2 book 1",
-                        "Highlight (yellow) - heading 3 book 1"
+        runTest {
+            whenever(booksParser.parseHtml(any())).thenReturn(
+                mutableListOf(
+                    HtmlDocument(
+                        heading = listOf(
+                            "Highlight (yellow) - heading 1 book 1",
+                            "Highlight (yellow) - heading 2 book 1",
+                            "Highlight (yellow) - heading 3 book 1"
+                        ),
+                        highlight = listOf(
+                            "highlight 1 book 1",
+                            "highlight 2 book 1",
+                            "highlight 3 book 1"
+                        ),
+                        bookName = "Book name 1"
                     ),
-                    highlight = listOf(
-                        "highlight 1 book 1",
-                        "highlight 2 book 1",
-                        "highlight 3 book 1"
-                    ),
-                    bookName = "Book name 1"
-                ),
+                )
             )
-        )
-        init()
+            init()
 
-        (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
+            (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
 
-        assertThat(viewModel.viewState.value).isInstanceOf(
-            BooksListUIState.Success::class.java
-        )
+            assertThat(viewModel.viewState.value).isInstanceOf(
+                BooksListUIState.Success::class.java
+            )
+        }
     }
 
     @Test
     fun `given storage permission granted, when books found on storage, then success state with correct data is shown`() {
-        whenever(booksParser.parseHtml(any())).thenReturn(
-            mutableListOf(
-                HtmlDocument(
-                    heading = listOf(
-                        "Highlight (yellow) - heading 1 book 1",
-                        "Highlight (yellow) - heading 2 book 1",
-                        "Highlight (yellow) - heading 3 book 1"
+        runTest {
+
+            whenever(booksParser.parseHtml(any())).thenReturn(
+                mutableListOf(
+                    HtmlDocument(
+                        heading = listOf(
+                            "Highlight (yellow) - heading 1 book 1",
+                            "Highlight (yellow) - heading 2 book 1",
+                            "Highlight (yellow) - heading 3 book 1"
+                        ),
+                        highlight = listOf(
+                            "highlight 1 book 1",
+                            "highlight 2 book 1",
+                            "highlight 3 book 1"
+                        ),
+                        bookName = "Book name 1"
                     ),
-                    highlight = listOf(
-                        "highlight 1 book 1",
-                        "highlight 2 book 1",
-                        "highlight 3 book 1"
-                    ),
-                    bookName = "Book name 1"
-                ),
+                )
             )
-        )
-        val expectedSuccessState = BooksListUIState.Success(
-            booksList = listOf(
-                Book(
-                    title = "Book name 1",
-                    highlights = mutableListOf(
-                        Highlight(
-                            line = "",
-                            heading = "heading 1 book 1",
-                            highlightText = "highlight 1 book 1"
-                        ),
-                        Highlight(
-                            line = "",
-                            heading = "heading 2 book 1",
-                            highlightText = "highlight 2 book 1"
-                        ),
-                        Highlight(
-                            line = "",
-                            heading = "heading 3 book 1",
-                            highlightText = "highlight 3 book 1"
+            val expectedSuccessState = BooksListUIState.Success(
+                booksList = listOf(
+                    Book(
+                        title = "Book name 1",
+                        thumbnail = "",
+                        highlights = mutableListOf(
+                            Highlight(
+                                line = "",
+                                heading = "heading 1 book 1",
+                                highlightText = "highlight 1 book 1"
+                            ),
+                            Highlight(
+                                line = "",
+                                heading = "heading 2 book 1",
+                                highlightText = "highlight 2 book 1"
+                            ),
+                            Highlight(
+                                line = "",
+                                heading = "heading 3 book 1",
+                                highlightText = "highlight 3 book 1"
+                            )
                         )
                     )
                 )
             )
-        )
-        init()
+            init()
 
-        (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
+            (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
 
-        assertThat(viewModel.viewState.value).isEqualTo(expectedSuccessState)
+            assertThat(viewModel.viewState.value).isEqualTo(expectedSuccessState)
+        }
+    }
+
+    @Test
+    fun `given storage permission granted, when books found on storage, then GetBookDetails event is triggered`() {
+        runTest {
+
+            whenever(booksParser.parseHtml(any())).thenReturn(
+                mutableListOf(
+                    HtmlDocument(
+                        heading = listOf(
+                            "Highlight (yellow) - heading 1 book 1"
+                        ),
+                        highlight = listOf(
+                            "highlight 1 book 1"
+                        ),
+                        bookName = "Book name 1"
+                    ),
+                )
+            )
+            init()
+
+            (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
+
+            assertThat(viewModel.event.value).isInstanceOf(
+                GetBookDetails::class.java
+            )
+        }
     }
 
     @Test
     fun `given storage permission granted, when books NOT found on storage, then error state is shown`() {
-        whenever(booksParser.parseHtml(any())).thenReturn(mutableListOf())
-        init()
+        runTest {
+            whenever(booksParser.parseHtml(any())).thenReturn(mutableListOf())
+            init()
 
-        (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
+            (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
 
-        assertThat(viewModel.viewState.value).isInstanceOf(
-            BooksListUIState.Error::class.java
-        )
+            assertThat(viewModel.viewState.value).isInstanceOf(
+                BooksListUIState.Error::class.java
+            )
+        }
+    }
+
+    @Test
+    fun `given storage permission granted, when books NOT found on storage, then GetBookDetails event is not triggered`() {
+        runTest {
+            whenever(booksParser.parseHtml(any())).thenReturn(mutableListOf())
+            init()
+
+            (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
+
+            assertThat(viewModel.event.value).isNotInstanceOf(
+                GetBookDetails::class.java
+            )
+        }
     }
 
     @Test
     fun `given storage permission granted, when books NOT found on storage, then error state is shown with correct message`() {
-        whenever(booksParser.parseHtml(any())).thenReturn(mutableListOf())
-        init()
-        val expectedErrorMessage = "No books found in the specified location!"
-        (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
-        val actualErrorMessage = (viewModel.viewState.value as BooksListUIState.Error).error.message
+        runTest {
+            whenever(booksParser.parseHtml(any())).thenReturn(mutableListOf())
+            init()
+            val expectedErrorMessage = "No books found in the specified location!"
+            (viewModel.event.value as ParseBooksFromStorage).parseBooks.invoke(listOf())
+            val actualErrorMessage = (viewModel.viewState.value as BooksListUIState.Error).error.message
 
-        assertThat(actualErrorMessage).isEqualTo(expectedErrorMessage)
+            assertThat(actualErrorMessage).isEqualTo(expectedErrorMessage)
+        }
     }
 }
