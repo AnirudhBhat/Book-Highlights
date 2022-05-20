607bbd4a6346a5d7f850ba68f264e483b723fd9b
Anirudh Uppunda <abhat38@gmail.com>
2022-05-20 09:44:43 +0530

    make book model parcelable

--- app/src/main/java/com/abhat/bookhighlights/bookslist/model/Book.kt
+++ app/src/main/java/com/abhat/bookhighlights/bookslist/model/Book.kt
@@ -1,13 +1,18 @@
 package com.abhat.bookhighlights.bookslist.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Book(
     val title: String,
     val thumbnail: String,
     val highlights: MutableList<Highlight> = mutableListOf(),
-)
+) : Parcelable
 
+@Parcelize
 data class Highlight(
     val line: String,
     val heading: String = "",
     val highlightText: String
-)
+) : Parcelable
